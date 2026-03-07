e46000c2c168c62e0bc6e1907b646843086914c4
NicoleRamirez46 <nicolejulayrs@gmail.com>
2026-03-06 20:57:11 -0500

    Primer programa

---
+++ semana3/ejer1.py
@@ -0,0 +1,15 @@
+#esto es un tipo de dato float
+a=20
+#esto es un tipo de dato entero 
+b=3
+print("la suma es:",a+b)
+print("la resta es:",a-b)
+print("la multiplicacion es:",a*b)
+print("la division es:",a/b)
+print("la division entera es:",a//b)
+#el modulo es el residuo de la division 
+print("el residuo es:",a%b)
+#comparaciones
+print(a<b)
+print(a>b)
+print(a==b)
