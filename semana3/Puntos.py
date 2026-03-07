a365af7d6b6ccde1f8f0ff9757298b49042c92d1
NicoleRamirez46 <nicolejulayrs@gmail.com>
2026-03-06 21:12:16 -0500

    Creacion puntaje.py

---
+++ semana3/Puntos.py
@@ -0,0 +1,14 @@
+#ALGORITMO
+#PEDIR EL PUNTAJE 1
+#PEDIR EL PUNTAJE 2
+#COMPARAR PUNTAJE 1 CON PUNTAJE 2
+
+#PSEUDOCODIGO
+#INICIO
+#LEER PUNTAJE 1
+#LEER PUNTAJE 2
+#ESCRIBIR SI PUNTAJE 1 ES MAYOR QUE PUNTAJE 2
+
+puntaje1= int(input("Ingrese primer puntaje: "))
+puntaje2= int(input("Ingrese el segundo puntaje: "))
+print(puntaje1 > puntaje2)
