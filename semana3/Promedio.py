dc9fbf1bc6e3a278e784c70f0ab2ce898669b8f2
NicoleRamirez46 <nicolejulayrs@gmail.com>
2026-03-06 23:55:41 -0500

    Promedio.py

---
+++ semana3/Promedio.py
@@ -0,0 +1,21 @@
+#ALGORITMO 
+#Pedir la nota 1
+#Pedir la nota 2
+#Pedir la nota 3
+#Sumar las tres notas
+#Dividir las notas en 3
+
+#PSEUDOCODIGO
+#Inicio
+#Leer la nota 1
+#Leer la nota 2
+#Leer la nota 3
+#Promedio (nota1+nota2+nota3)/3
+#Escribir el promedio es:
+
+nota1 = float(input("Ingrese la primera nota: "))
+nota2 = float(input("Ingrese la segunda nota: "))
+nota3 = float(input("Ingrese la tercera nota: "))
+
+Promedio = (nota1 + nota2 + nota3)/3
+print("El promedio es: ", Promedio) 
