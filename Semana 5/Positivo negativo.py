948d6ab031ee47b4142ef4ac220691b8c109f7a2
NicoleRamirez46 <nicolejulayrs@gmail.com>
2026-03-18 00:22:32 -0500

    Positivo negativo.py

--- Semana 5/Positivo negativo.py
+++ Semana 5/Positivo negativo.py
@@ -1,4 +1,25 @@
-#Numero positivo, negativo o cero 
+#Numero positivo, negativo o cero
+#ALGORITMO
+#1. Pedir el número
+#2. Mostrar número 
+#3 Comparar si el numero es > que 0 
+#4. Mostrar que el numero es positivo
+#5. Si el numero es < que cero 
+#6. Mostrar que el numero es negativo
+#7. Si el numero es = a 0
+#8. Mostrar que el numero es neutro
+#PSEUDOCODIGO
+#INICIO
+    #ESCRIBIR "Ingrese el numero"
+    #LEER numero
+    #Si numero > que 0
+    #ESCRIBIR "Numero positivo"
+    #Si numero < que 0
+    #ESCRIBIR "Numero negativo"
+    #Sino 
+    #ESCRIBIR "Numero neutro"
+    #FinSi
+#FIN 
 numero=int(input("Ingrese numero: "))
 if numero > 0:
   print ("El numero es positivo")
