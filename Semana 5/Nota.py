b2647554944664bc72765e1a1a09b1b871f92c82
NicoleRamirez46 <nicolejulayrs@gmail.com>
2026-03-18 00:38:03 -0500

    Nota.py

--- Semana 5/Nota.py
+++ Semana 5/Nota.py
@@ -3,21 +3,18 @@
 #Leer la nota
 #Si la nota es >= a 3.0
 #Mostrar Apruebas la materia, felicidades
-#Si no 
+#Sino 
 #Mostrar Reprobaste la materia. Debes presentar la recuperación
 
 #PSEUDOCODIGO
 #INICIO
-        #
-
-
-
-
-
-
-
-
-
+        #ESCRIBIR "Ingrese la nota final: "
+        #LEER nota
+        #Si nota >=3.0 Entonces
+        #ESCRIBIR "Aprobaste la materia, felicidades."
+        #Sino
+        #ESCRIBIR "Reprobaste la materia. Debes presentar la recuperación"
+#FIN
 
 nota=float(input("Digite su nota "))
 if nota >= 3.0: 
