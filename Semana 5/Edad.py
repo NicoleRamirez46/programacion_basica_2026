67c46d3892d70f35e0b4cd9ab5d77b9b5614ef82
NicoleRamirez46 <nicolejulayrs@gmail.com>
2026-03-18 00:08:24 -0500

    Edad.py

--- Semana 5/Edad.py
+++ Semana 5/Edad.py
@@ -1,3 +1,20 @@
+#ALGORITMO
+#1. El usuario ingresa la edad
+#2. Mostrar la edad
+#3. Comparar si la edad es >=18
+#4. Mostrar Puede ingresar a la discoteca
+#5. Si no
+#6. Mostrar "No puede ingresar a la discoteca"
+#PSEUDOCODIGO
+#INICIO
+  #ESCRIBIR "Ingrese su edad: "
+  #LEER edad
+  #Si edad es >= 18 Entonces
+  #Escribir "Puede ingresar a la discoteca"
+  #Sino
+  #Escribir "No puede ingresar a la discoteca"
+  #FinSi
+#FIN
 edad=int(input("Ingrese su edad "))
 if edad >= 18:
  print("Puedes entrar al a distoteca")
