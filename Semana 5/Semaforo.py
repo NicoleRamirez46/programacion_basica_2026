3f3e2b680629378db5e8efe117dd390523bda463
NicoleRamirez46 <nicolejulayrs@gmail.com>
2026-03-18 00:44:42 -0500

    Semaforo.py

--- Semana 5/Semaforo.py
+++ Semana 5/Semaforo.py
@@ -1,7 +1,30 @@
+#ALGORITMO
+#1. Pedir el color
+#2. Mostrar color
+#3 Comparar si el color es Verde 
+#4. Mostrar que puede continuar
+#5. Si el color es Amarillo
+#6. Mostrar que debe tener precaución
+#7. Si el color es Rojo
+#8. Mostrar que debe detenerse
+
+#PSEUDOCODIGO
+#INICIO
+    #ESCRIBIR "Ingrese el color"
+    #LEER color
+    #Si color es Verde
+    #ESCRIBIR "Puede continuar"
+    #Si color es Amarillo
+    #ESCRIBIR "Tenga precaución"
+    #Si color es Rojo
+    #ESCRIBIR "Debe detenerse"
+    #FinSi
+#FIN 
+
 semaforo=input("Dame el color del semaforo: ")
 if semaforo=="rojo":
- print ("Detenerse")
+ print ("Debe detenerse")
 elif semaforo=="amarillo":
-  print ("Precaucion")
+  print ("Tenga precaucion")
 elif semaforo=="verde":
-  print ("Continuar")
+  print ("Puede continuar")
