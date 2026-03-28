9d515e87e5be10fb9523b5f04e25067b85537e37
NicoleRamirez46 <nicolejulayrs@gmail.com>
2026-03-28 09:17:14 -0500

    ViajeAlExterior.py

--- Semana_7/ViajeAlExterior.py
+++ Semana_7/ViajeAlExterior.py
@@ -2,7 +2,7 @@ Pasaporte= input("¿Tienes pasaporte vigente?(si/no): ")
 Visa= input("Tienes visa (si/no): ")
 Exento= input("Tu país esta exento de Visa: ")
 
-if Pasaporte == "si" and Visa == "si" or Exento == "si": 
+if Pasaporte == "si" and (Visa == "si" or Exento == "si"): 
     print ("Puedes viajar. !Buen viaje :D¡")
 else:
     print ("No puedes viajar. Revisa tus documentos :c")
